f4a11a89b70b4f4be9c5f4ea8dfd30d65dd056d6
Arian Baishya <arian.baishya2004@gmail.com>
2024-10-12 15:31:34 -0500

    style: explicitly specify types of project-wide constants

--- shared/src/commonMain/kotlin/me/arianb/storm_robot/Constants.kt
+++ shared/src/commonMain/kotlin/me/arianb/storm_robot/Constants.kt
@@ -1,27 +1,32 @@
 package me.arianb.storm_robot
 
+// typealias-ing this in case I want to change all their types in the future
+typealias EndpointPath = String
+
 data object Server {
-    const val HOST = "localhost"
-    const val PORT = 8080
+    const val HOST: String = "localhost"
+    const val PORT: Int = 8080
 
     const val PING_PERIOD_MILLIS: Long = 15_000
     const val TIMEOUT_MILLIS: Long = 15_000
 
     data object Endpoints {
-        const val API_ROOT = "/api"
+        const val API_ROOT: EndpointPath = "/api"
 
         // TODO: add endpoint sub-paths
-        const val API_WHEELS = "/wheels"
-        const val API_ARM = "/arm"
-        const val API_IR_MESSAGES = "/messages"
+        const val API_WHEELS: EndpointPath = "/wheels"
+        const val API_ARM: EndpointPath = "/arm"
+        const val API_IR_MESSAGES: EndpointPath = "/messages"
 
-        const val VIDEO = "/video"
+        const val VIDEO: EndpointPath = "/video"
     }
 }
 
+typealias ResolutionSize = Int
+
 data object CAMERA {
-    const val RESOLUTION_WIDTH = 1280
-    const val RESOLUTION_HEIGHT = 720
-    const val EXPECTED_FPS = 30
-    const val ASPECT_RATIO = 16f / 9f
+    const val RESOLUTION_WIDTH: ResolutionSize = 1280
+    const val RESOLUTION_HEIGHT: ResolutionSize = 720
+    const val EXPECTED_FPS: Int = 30
+    const val ASPECT_RATIO: Float = 16f / 9f
 }
