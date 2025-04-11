f4a3e16e6f524e0cb1664d54e9bf6cef38d98915
Arian Baishya <arian.baishya2004@gmail.com>
2025-04-11 12:44:28 -0500

    separate route paths and implementations

--- server/src/main/kotlin/me/arianb/storm_robot/Wheels.kt
+++ server/src/main/kotlin/me/arianb/storm_robot/Wheels.kt
@@ -4,33 +4,38 @@ import io.ktor.serialization.deserialize
 import io.ktor.server.application.log
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.application
+import io.ktor.server.routing.route
 import io.ktor.server.websocket.converter
 import io.ktor.server.websocket.webSocket
 import io.ktor.util.logging.error
 import kotlinx.coroutines.channels.ClosedReceiveChannelException
 
 fun Route.wheelsRoutes() {
-    webSocket("/move") {
-        try {
-            for (frame in incoming) {
-                val movementPacket = converter?.deserialize<WheelMovementPacket>(frame)
-                    ?: throw RuntimeException("failed to deserialize WheelMovementPacket, websocket converter is null")
-                application.log.debug("received: {}", movementPacket)
+    route("/move") {
+        move()
+    }
+}
 
-                MicrocontrollerSerialConnection.send(movementPacket)
-            }
-        } catch (e: ClosedReceiveChannelException) {
-            // ensure motors are stopped on client disconnection
-            MicrocontrollerSerialConnection.send(WheelMovementPacket(0, 0, 0))
+private fun Route.move() = webSocket {
+    try {
+        for (frame in incoming) {
+            val movementPacket = converter?.deserialize<WheelMovementPacket>(frame)
+                ?: throw RuntimeException("failed to deserialize WheelMovementPacket, websocket converter is null")
+            application.log.debug("received: {}", movementPacket)
 
-            application.log.info(
-                "client disconnected from wheels websocket, reason: {}",
-                closeReason.await()
-            )
-        } catch (e: Throwable) {
-            application.log.error("onError {}", closeReason.await())
-            application.log.error(e)
+            MicrocontrollerSerialConnection.send(movementPacket)
         }
+    } catch (e: ClosedReceiveChannelException) {
+        // ensure motors are stopped on client disconnection
+        MicrocontrollerSerialConnection.send(WheelMovementPacket(0, 0, 0))
+
+        application.log.info(
+            "client disconnected from wheels websocket, reason: {}",
+            closeReason.await()
+        )
+    } catch (e: Throwable) {
+        application.log.error("onError {}", closeReason.await())
+        application.log.error(e)
     }
 }
 
