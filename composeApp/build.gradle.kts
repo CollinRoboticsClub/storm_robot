8e081389af82d12d7ebc6ff9aa1847215aba828c
Arian Baishya <arian.baishya2004@gmail.com>
2025-10-21 19:03:41 -0500

    fixup! chore: bump dependencies and gradle version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -32,6 +32,7 @@ kotlin {
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
+            implementation(libs.compose.materialIconsCore)
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.viewmodel.compose)
             implementation(libs.androidx.lifecycle.runtime.compose)
